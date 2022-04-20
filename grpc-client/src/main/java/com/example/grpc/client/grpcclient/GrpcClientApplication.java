099046fa6963a76db806e49065ff46b732e29b55
@BroBro92166 <blkngoy@gmail.com>
2022-04-20 20:04:30 +0100

    commit #2

--- grpc-client/src/main/java/com/example/grpc/client/grpcclient/GrpcClientApplication.java
+++ grpc-client/src/main/java/com/example/grpc/client/grpcclient/GrpcClientApplication.java
@@ -8,10 +8,13 @@ import io.grpc.ManagedChannelBuilder;
 import net.devh.boot.grpc.client.inject.GrpcClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
 
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {ErrorMvcAutoConfiguration.class})
+@ComponentScan(basePackages = ("com.example.grpc.client.grpcclient"))
 public class GrpcClientApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
